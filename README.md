684d81990da6db7314d985de33eec938f0f93b07
Roberto <roberto.molli@vodafone.com>
2023-01-12 15:48:28 +0100

    Completed the first version with the right TAX Calculation

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
-# uktaxincome
-It is my first example of web assembly. I decided to create a simple page that calculates your tax income ... 
+# Uk_tax_income 
+It is my first example of web assembly. I decided to create a simple page that calculates your uk tax income ... 
 
 
-# Compile
+# How to compile Compile
+
+``` bash
+emcc ukTaxCalculator.c  -o output/ukTaxCalculator.js 
+```
+
+## examples 
 emcc helloworld.c -o hello.html
 
 emcc helloworld.c -o output/hello5.html --shell-file ./html_template/my_template.html -s NO_EXIT_RUNTIME=1 -s "EXPORTED_RUNTIME_METHODS=['ccall']"
 
 emcc helloworld.c -o output/hello5.html --shell-file ./html_template/my_template.html -s NO_EXIT_RUNTIME=1 -s "EXPORTED_RUNTIME_METHODS='ccall','cwrap'"
 
-# Run a WebServer
+
+# Run a WebServer on local
 python3 -m http.server --cgi 8080
 
 # docs
 
-[]{https://www.youtube.com/watch?v=RSLHrmDL4kU}
+[Really good tutorial](https://www.youtube.com/watch?v=RSLHrmDL4kU)
+
+[Nice explanation tutorial](https://marcoselvatici.github.io/WASM_tutorial/)
---
+++ ukTaxCalculator.c
@@ -0,0 +1,79 @@
+#include <float.h>
+#include <emscripten.h>
+
+
+/*
+    =================================================
+    Band 	           | Taxable income 	| Tax rate
+    ==================================================
+    Personal Allowance | Up to £12,570 	    |    0%
+    Basic rate         | £12,571 to £50,270 |	20%
+    Higher rate        | £50,271 to £150,000| 	40%
+    Additional rate    | over £150,000 	    |   45%
+
+    SPACIAL CASE:
+    You do not get a Personal Allowance on taxable income over £125,140.
+    The Personal Allowance goes down by £1 for every £2 of income above the £100,000 limit. 
+    It can go down to zero.
+*/
+
+
+const float MAX_Personal_Allowance = 12570;
+const float MAX_Basic_rate = 50270;
+const float MAX_Higher_rate = 150000;
+const float MAX_Additional = FLT_MAX;
+const float MAX_Personal_Allowance_Removed = 125140;
+
+const float MAX_Personal_Allowance_tax = 0;
+const float MAX_Basic_rate_tax = 0.20;
+const float MAX_Higher_rate_tax = 0.40;
+const float MAX_Additional_tax = 0.45;
+
+// those are the UK task steps.
+float taxRates[] = {0, MAX_Personal_Allowance, MAX_Basic_rate, MAX_Higher_rate, MAX_Additional};
+
+// these are the tax to pay for each steps
+float taxs[] = {0, MAX_Personal_Allowance_tax, MAX_Basic_rate_tax, MAX_Higher_rate_tax, MAX_Additional_tax};
+
+
+EMSCRIPTEN_KEEPALIVE float howMuchTaxes(float income)
+{
+
+    int index = 1;
+    float taxToPay = 0;
+    int foundMax = 0;
+
+    // printf("You income is \n");
+    float incomeToApplyTaxes = income;
+
+    while (foundMax == 0 && index < 4)
+    {
+        if (income > taxRates[index])
+        {
+            taxToPay += (taxRates[index] - taxRates[index - 1]) * taxs[index];
+        }
+        else
+        {
+            incomeToApplyTaxes = income - taxRates[index - 1];
+            taxToPay += (incomeToApplyTaxes)*taxs[index];
+            foundMax = 1;
+        }
+        index++;
+    }
+
+
+    //Special Case
+    if (income > 100000)
+    {
+        if (income > MAX_Personal_Allowance_Removed)
+        {
+            taxToPay += MAX_Personal_Allowance;
+        }else{
+            taxToPay += (income-100000)*0.5;
+        }
+    }
+
+    // printf("All considered you will pay %f", taxToPay);
+
+    return taxToPay;
+}
